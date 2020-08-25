0f53d4575065d417c15d663ef0fc56edd6899eef
Andy Tran <xpandalord@gmail.com>
2020-08-25 16:31:49 -0400

    updates

--- app/api/predict.py
+++ app/api/predict.py
@@ -9,44 +9,19 @@ log = logging.getLogger(__name__)
 router = APIRouter()
 
 
-class Item(BaseModel):
+class AirBnB(BaseModel):
     """Use this data model to parse the request body JSON."""
-
-    x1: float = Field(..., example=3.14)
-    x2: int = Field(..., example=-42)
-    x3: str = Field(..., example="banjo")
+    room_type: str
+    availability_365: int
+    neighbourhood: str
 
     def to_df(self):
         """Convert pydantic object to pandas dataframe with 1 row."""
         return pd.DataFrame([dict(self)])
 
-    @validator("x1")
-    def x1_must_be_positive(cls, value):
-        """Validate that x1 is a positive number."""
-        assert value > 0, f"x1 == {value}, must be > 0"
-        return value
-
-
-@router.post("/predict")
-async def predict(item: Item):
-    """
-    Make random baseline predictions for classification problem 🔮
-
-    ### Request Body
-    - `x1`: positive float
-    - `x2`: integer
-    - `x3`: string
-
-    ### Response
-    - `prediction`: boolean, at random
-    - `predict_proba`: float between 0.5 and 1.0, 
-    representing the predicted class's probability
-
-    Replace the placeholder docstring and fake predictions with your own model.
-    """
-
-    X_new = item.to_df()
-    log.info(X_new)
-    y_pred = random.choice([True, False])
-    y_pred_proba = random.random() / 2 + 0.5
-    return {"prediction": y_pred, "probability": y_pred_proba}
+@router.post('/predict')
+async def predict(AirBnB: AirBnB):
+    """Predict AirBnB prices in NYC."""
+    X_new = AirBnB.to_df()
+    y_pred = 200000
+    return {'predicted_price $': y_pred}
