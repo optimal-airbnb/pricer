70fe7ac78e0735271801a23876ba7fdada6ee17c
Sean Byrne <ssbyrne89@gmail.com>
2020-08-25 16:41:54 -0600

    visual

--- app/api/viz.py
+++ app/api/viz.py
@@ -4,24 +4,27 @@ import plotly.express as px
 
 router = APIRouter()
 
-df = pd.read_csv('pricer\app\api\clean_rent.csv')
+df = pd.read_csv("assets/clean_rent.csv")
 
 @router.get('/map')
 async def visual():
-  """practice visual to test the heroku deployment """
-  fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", color="price",
-                          hover_data=["neighbourhood_group",	"neighbourhood", "id"],
-                          color_discrete_sequence=["fuchsia"], 
-                          zoom=10, height=125)
+    """
+    this takes the cleaned NYC rental dataset in csv format and makes interactive heatmap 
 
-  # Location Graph
-  fig.update_layout(mapbox_style="open-street-map")
+    ### Response
+    JSON string to render with react-plotly.js
+    """
+    fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", color="price",
+                            hover_data=["neighbourhood_group",	"neighbourhood", "id"],
+                            color_discrete_sequence=["fuchsia"], 
+                            zoom=10, height=125)
 
-  # Layout Features
-  fig.update_layout(width=1000, 
+    # Location Graph
+    fig.update_layout(mapbox_style="open-street-map")
+
+    # Layout Features
+    fig.update_layout(width=1000, 
                     height=1000, 
                     margin={"r":1,"t":1,"l":1,"b":1})
 
-  # Show Graphic
-  fig.show();
-  return fig.to_json()
+    return fig.to_json()
