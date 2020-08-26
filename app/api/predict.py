766a804228de86d46a5c2e81e370eb08c9922a20
Andy Tran <xpandalord@gmail.com>
2020-08-25 20:59:33 -0400

    updates

--- app/api/predict.py
+++ app/api/predict.py
@@ -5,16 +5,23 @@ from fastapi import APIRouter
 import pandas as pd
 from pydantic import BaseModel, Field, validator
 from joblib import load
+import os
 
 log = logging.getLogger(__name__)
 router = APIRouter()
 
+filepath = os.path.join(os.path.dirname(__file__), "..",
+                                     "assets",
+                                     "rf_reg.joblib")
+
 
 class AirBnB(BaseModel):
     """Use this data model to parse the request body JSON."""
-    room_type: str
-    availability_365: int
-    neighbourhood: str
+    Borough: str = Field(..., example="Brooklyn")
+    Neighbourhood: str = Field(..., example="Kensington")
+    Room_type: str = Field(..., example="Private room")
+    Minimum_nights: int = Field(..., example=1)
+    Availability_365: int = Field(..., example=365)
 
     def to_df(self):
         """Convert pydantic object to pandas dataframe with 1 row."""
@@ -23,7 +30,8 @@ class AirBnB(BaseModel):
 @router.post('/predict')
 async def predict(AirBnB: AirBnB):
     """Predict AirBnB prices in NYC."""
-    X_new = AirBnB.to_df()
-    pipeline = load("assets/rf_reg.joblib")
-    y_pred = pipeline.predict(X_new)[0]
-    return {'predicted_price $': y_pred}
+    df = AirBnB.to_df()
+    pipeline = load(filepath)
+    #pipeline = load('assets/rf_reg.joblib')
+    return pipeline.predict(df)
+    #return {'predicted_price $': y_pred}
